80179c891cc7794dbaebf4c3f8f0e204db8e1050
KattyLearner <ekaterina.derzhitskaya@gmail.com>
2021-09-01 19:43:59 +0300

    created cart-item component and connected our cart dropdown

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -3,6 +3,7 @@ import './cart-dropdown.styles.scss'
 import CustomButton from "../custom-button/custom-button.component";
 import {connect} from "react-redux";
 import CartItem from "../cart-item/cart-item.component";
+import {selectCartItems} from "../../redux/cart/cart.selectors";
 
 
 const CartDropdown = ({cartItems}) => {
@@ -17,7 +18,7 @@ const CartDropdown = ({cartItems}) => {
         </div>
     )
 }
- const mapStateTpProps = ({cart: {cartItems}}) => ({
-     cartItems
+ const mapStateTpProps = (state) => ({
+     cartItems: selectCartItems(state)
  })
 export default connect(mapStateTpProps)(CartDropdown)
